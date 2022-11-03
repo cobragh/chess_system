945fa425141a6a5e493fbc53e415e0844e908e1f
cobragh <lucas_alvesf09@outlook.com>
2022-11-03 17:17:18 -0300

    "position"

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -1,7 +1,11 @@
 package application;
 
+import boardgame.Positions;
+
 public class Program {
     public static void main(String[] args){
-        
+
+        Positions pos = new Positions(16, 10);
+        System.out.println(pos);
     }
 }
